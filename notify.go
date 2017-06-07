ea6bd5b7fa949a71ba2fa5b3c45099aa8c1db6d1
wuqunlin <wuqunlin@g7.com.cn>
2017-06-07 18:16:51 +0800

    minimax relay

--- notify.go
+++ notify.go
@@ -1,18 +1,36 @@
 package alipay
 
 import (
+	"io"
 	"io/ioutil"
+	"net/http"
 	"net/url"
-
-	"github.com/smartwalle/going/request"
+	"strings"
 )
+
+func NewRequest(method, url string, params url.Values) (*http.Request, error) {
+	var m = strings.ToUpper(method)
+	var body io.Reader
+	if m == "GET" || m == "HEAD" {
+		if len(params) > 0 {
+			if strings.Contains(url, "?") {
+				url = url + "&" + params.Encode()
+			} else {
+				url = url + "?" + params.Encode()
+			}
+		}
+	} else {
+		body = strings.NewReader(params.Encode())
+	}
+	return http.NewRequest(m, url, body)
+}
 
 func (this *AliPay) NotifyVerify(notifyId string) bool {
 	var param = url.Values{}
 	param.Add("service", "notify_verify")
 	param.Add("partner", this.partnerId)
 	param.Add("notify_id", notifyId)
-	req, err := request.NewRequest("GET", this.apiDomain, param)
+	req, err := NewRequest("GET", this.apiDomain, param)
 	if err != nil {
 		return false
 	}
