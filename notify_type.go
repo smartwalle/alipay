dfec030c2d8b57bf600d2d1026e4ed5f3a55ebf1
SmartWalle <smartwalle@gmail.com>
2018-04-08 10:49:19 +0800

    update

--- notify_type.go
+++ notify_type.go
@@ -1,10 +1,5 @@
 package alipay
 
-import (
-	"errors"
-	"net/http"
-)
-
 // https://doc.open.alipay.com/docs/doc.htm?spm=a219a.7629140.0.0.8AmJwg&treeId=203&articleId=105286&docType=1
 type TradeNotification struct {
 	AuthAppId         string `json:"auth_app_id"`         // App Id
@@ -39,58 +34,4 @@ type TradeNotification struct {
 	FundBillList      string `json:"fund_bill_list"`      // 支付金额信息
 	PassbackParams    string `json:"passback_params"`     // 回传参数
 	VoucherDetailList string `json:"voucher_detail_list"` // 优惠券信息
-}
-
-func (this *AliPay) GetTradeNotification(req *http.Request) (*TradeNotification, error) {
-	return GetTradeNotification(req, this.AliPayPublicKey)
-}
-
-func GetTradeNotification(req *http.Request, aliPayPublicKey []byte) (noti *TradeNotification, err error) {
-	if req == nil {
-		return nil, errors.New("request 参数不能为空")
-	}
-
-	noti = &TradeNotification{}
-	noti.AppId = req.FormValue("app_id")
-	noti.AuthAppId = req.FormValue("auth_app_id")
-	noti.NotifyId = req.FormValue("notify_id")
-	noti.NotifyType = req.FormValue("notify_type")
-	noti.NotifyTime = req.FormValue("notify_time")
-	noti.TradeNo = req.FormValue("trade_no")
-	noti.TradeStatus = req.FormValue("trade_status")
-	noti.TotalAmount = req.FormValue("total_amount")
-	noti.ReceiptAmount = req.FormValue("receipt_amount")
-	noti.InvoiceAmount = req.FormValue("invoice_amount")
-	noti.BuyerPayAmount = req.FormValue("buyer_pay_amount")
-	noti.SellerId = req.FormValue("seller_id")
-	noti.SellerEmail = req.FormValue("seller_email")
-	noti.BuyerId = req.FormValue("buyer_id")
-	noti.BuyerLogonId = req.FormValue("buyer_logon_id")
-	noti.FundBillList = req.FormValue("fund_bill_list")
-	noti.Charset = req.FormValue("charset")
-	noti.PointAmount = req.FormValue("point_amount")
-	noti.OutTradeNo = req.FormValue("out_trade_no")
-	noti.OutBizNo = req.FormValue("out_biz_no")
-	noti.GmtCreate = req.FormValue("gmt_create")
-	noti.GmtPayment = req.FormValue("gmt_payment")
-	noti.GmtRefund = req.FormValue("gmt_refund")
-	noti.GmtClose = req.FormValue("gmt_close")
-	noti.Subject = req.FormValue("subject")
-	noti.Body = req.FormValue("body")
-	noti.RefundFee = req.FormValue("refund_fee")
-	noti.Version = req.FormValue("version")
-	noti.SignType = req.FormValue("sign_type")
-	noti.Sign = req.FormValue("sign")
-	noti.PassbackParams = req.FormValue("passback_params")
-	noti.VoucherDetailList = req.FormValue("voucher_detail_list")
-
-	if len(noti.NotifyId) == 0 {
-		return nil, errors.New("不是有效的 Notify")
-	}
-
-	ok, err := verifySign(req.Form, aliPayPublicKey)
-	if ok == false {
-		return nil, err
-	}
-	return noti, err
 }
