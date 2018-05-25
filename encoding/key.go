98d1c299d0f4ddd4d22f16d69d4838c7e821fab1
melodysong <mrsong0607@gmail.com>
2018-05-25 14:12:06 +0800

    fixs "private key error"

--- encoding/key.go
+++ encoding/key.go
@@ -1,8 +1,8 @@
 package encoding
 
 import (
-	"strings"
 	"bytes"
+	"strings"
 )
 
 func ParsePublicKey(raw string) (result []byte) {
@@ -14,27 +14,23 @@ func ParsePrivateKey(raw string) (result []byte) {
 }
 
 func parseKey(raw, prefix, suffix string) (result []byte) {
-	if strings.HasPrefix(raw, prefix) {
-		raw = strings.Replace(raw, prefix, "", 1)
-	}
-	if strings.HasSuffix(raw, suffix) {
-		raw = strings.Replace(raw, suffix, "", 1)
-	}
-
+	raw = strings.Replace(raw, prefix, "", 1)
+	raw = strings.Replace(raw, suffix, "", 1)
 	raw = strings.Replace(raw, " ", "", -1)
 	raw = strings.Replace(raw, "\n", "", -1)
 	raw = strings.Replace(raw, "\r", "", -1)
+	raw = strings.Replace(raw, "\t", "", -1)
 
 	var ll = 64
 	var sl = len(raw)
 	var c = sl / ll
-	if sl % ll > 0 {
+	if sl%ll > 0 {
 		c = c + 1
 	}
 
 	var buf bytes.Buffer
-	buf.WriteString(prefix+"\n")
-	for i:=0; i<c; i++ {
+	buf.WriteString(prefix + "\n")
+	for i := 0; i < c; i++ {
 		var b = i * ll
 		var e = b + ll
 		if e > sl {
