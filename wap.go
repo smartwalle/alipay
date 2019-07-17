10de2186e4dada4a64c174ba5e0ca27fada0804d
smartwalle <smartwalle@gmail.com>
2019-07-17 16:45:38 +0800

    修正 TradeWapPay 接口

--- wap.go
+++ wap.go
@@ -1,34 +1,10 @@
 package alipay
 
-import (
-	"net/http"
-	"net/url"
-	"strings"
-)
-
 // TradeWapPay https://docs.open.alipay.com/api_1/alipay.trade.wap.pay/
-func (this *Client) TradeWapPay(param TradeWapPay) (url *url.URL, err error) {
+func (this *Client) TradeWapPay(param TradeWapPay) (url string, err error) {
 	p, err := this.URLValues(param)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	var buf = strings.NewReader(p.Encode())
-
-	req, err := http.NewRequest("POST", this.apiDomain, buf)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", kContentType)
-
-	rep, err := this.Client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer rep.Body.Close()
-
-	if err != nil {
-		return nil, err
-	}
-	url = rep.Request.URL
-	return url, err
+	return this.apiDomain + "?" + p.Encode(), nil
 }
