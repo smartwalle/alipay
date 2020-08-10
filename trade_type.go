ac1ca9ff43c0aa56b8fc8c87b6b42571d64f40cb
smartwalle <smartwalle@gmail.com>
2020-08-10 09:35:58 +0800

    修正 ExtendParams 字段数据类型

--- trade_type.go
+++ trade_type.go
@@ -16,19 +16,19 @@ type Trade struct {
 	DisablePayChannels string `json:"disable_pay_channels,omitempty"` // 禁用渠道，用户不可用指定渠道支付 当有多个渠道时用“,”分隔 注，与enable_pay_channels互斥
 	EnablePayChannels  string `json:"enable_pay_channels,omitempty"`  // 可用渠道，用户只能在指定渠道范围内支付  当有多个渠道时用“,”分隔 注，与disable_pay_channels互斥
 	//ExtUserInfo        string `json:"ext_user_info,omitempty"`        // 外部指定买家
-	ExtendParams     string `json:"extend_params,omitempty"`     // 业务扩展参数，详见下面的“业务扩展参数说明”
-	GoodsType        string `json:"goods_type,omitempty"`        // 商品主类型：0—虚拟类商品，1—实物类商品 注：虚拟类商品不支持使用花呗渠道
-	InvoiceInfo      string `json:"invoice_info,omitempty"`      // 开票信息
-	PassbackParams   string `json:"passback_params,omitempty"`   // 公用回传参数，如果请求时传递了该参数，则返回给商户时会回传该参数。支付宝会在异步通知时将该参数原样返回。本参数必须进行UrlEncode之后才可以发送给支付宝
-	PromoParams      string `json:"promo_params,omitempty"`      // 优惠参数 注：仅与支付宝协商后可用
-	RoyaltyInfo      string `json:"royalty_info,omitempty"`      // 描述分账信息，json格式，详见分账参数说明
-	SellerId         string `json:"seller_id,omitempty"`         // 收款支付宝用户ID。 如果该值为空，则默认为商户签约账号对应的支付宝用户ID
-	SettleInfo       string `json:"settle_info,omitempty"`       // 描述结算信息，json格式，详见结算参数说明
-	SpecifiedChannel string `json:"specified_channel,omitempty"` // 指定渠道，目前仅支持传入pcredit  若由于用户原因渠道不可用，用户可选择是否用其他渠道支付。  注：该参数不可与花呗分期参数同时传入
-	StoreId          string `json:"store_id,omitempty"`          // 商户门店编号。该参数用于请求参数中以区分各门店，非必传项。
-	SubMerchant      string `json:"sub_merchant,omitempty"`      // 间连受理商户信息体，当前只对特殊银行机构特定场景下使用此字段
-	TimeoutExpress   string `json:"timeout_express,omitempty"`   // 该笔订单允许的最晚付款时间，逾期将关闭交易。取值范围：1m～15d。m-分钟，h-小时，d-天，1c-当天（1c-当天的情况下，无论交易何时创建，都在0点关闭）。 该参数数值不接受小数点， 如 1.5h，可转换为 90m。
-	TimeExpire       string `json:"time_expire,omitempty"`       // 该笔订单绝对超时时间，格式为yyyy-MM-dd HH:mm:ss
+	ExtendParams     map[string]interface{} `json:"extend_params,omitempty"`     // 业务扩展参数，详见下面的“业务扩展参数说明”
+	GoodsType        string                 `json:"goods_type,omitempty"`        // 商品主类型：0—虚拟类商品，1—实物类商品 注：虚拟类商品不支持使用花呗渠道
+	InvoiceInfo      string                 `json:"invoice_info,omitempty"`      // 开票信息
+	PassbackParams   string                 `json:"passback_params,omitempty"`   // 公用回传参数，如果请求时传递了该参数，则返回给商户时会回传该参数。支付宝会在异步通知时将该参数原样返回。本参数必须进行UrlEncode之后才可以发送给支付宝
+	PromoParams      string                 `json:"promo_params,omitempty"`      // 优惠参数 注：仅与支付宝协商后可用
+	RoyaltyInfo      string                 `json:"royalty_info,omitempty"`      // 描述分账信息，json格式，详见分账参数说明
+	SellerId         string                 `json:"seller_id,omitempty"`         // 收款支付宝用户ID。 如果该值为空，则默认为商户签约账号对应的支付宝用户ID
+	SettleInfo       string                 `json:"settle_info,omitempty"`       // 描述结算信息，json格式，详见结算参数说明
+	SpecifiedChannel string                 `json:"specified_channel,omitempty"` // 指定渠道，目前仅支持传入pcredit  若由于用户原因渠道不可用，用户可选择是否用其他渠道支付。  注：该参数不可与花呗分期参数同时传入
+	StoreId          string                 `json:"store_id,omitempty"`          // 商户门店编号。该参数用于请求参数中以区分各门店，非必传项。
+	SubMerchant      string                 `json:"sub_merchant,omitempty"`      // 间连受理商户信息体，当前只对特殊银行机构特定场景下使用此字段
+	TimeoutExpress   string                 `json:"timeout_express,omitempty"`   // 该笔订单允许的最晚付款时间，逾期将关闭交易。取值范围：1m～15d。m-分钟，h-小时，d-天，1c-当天（1c-当天的情况下，无论交易何时创建，都在0点关闭）。 该参数数值不接受小数点， 如 1.5h，可转换为 90m。
+	TimeExpire       string                 `json:"time_expire,omitempty"`       // 该笔订单绝对超时时间，格式为yyyy-MM-dd HH:mm:ss
 }
 
 // TradePagePay 统一收单下单并支付页面接口请求参数 https://docs.open.alipay.com/api_1/alipay.trade.app.pay
@@ -388,13 +388,6 @@ type TradeCreateRsp struct {
 		OutTradeNo string `json:"out_trade_no"`
 	} `json:"alipay_trade_create_response"`
 	Sign string `json:"sign"`
-}
-
-type ExtendParamsItem struct {
-	SysServiceProviderId string `json:"sys_service_provider_id"`
-	HbFqNum              string `json:"hb_fq_num"`
-	HbFqSellerPercent    string `json:"hb_fq_seller_percent"`
-	TimeoutExpress       string `json:"timeout_express"`
 }
 
 type RoyaltyInfo struct {
