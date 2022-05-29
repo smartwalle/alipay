225336c40168ce62af826d9854d86b7c053e8860
wanglelecc <wanglelecc@gmail.com>
2022-05-29 15:47:16 +0800

    资金授权查询接口增加order_status

--- fund_type.go
+++ fund_type.go
@@ -277,40 +277,50 @@ func (this FundAuthOperationDetailQuery) Params() map[string]string {
 	return m
 }
 
+type OrderStatus string
+
+const (
+	OrderStatusInit       OrderStatus = "INIT"       //（初始状态：已创建未授权）
+	OrderStatusAuthorized OrderStatus = "AUTHORIZED" //（已授权状态：授权成功，可以进行转支付或解冻操作）
+	OrderStatusFinish     OrderStatus = "FINISH"     //（完成状态：转支付完成且无剩余冻结资金）
+	OrderStatusClosed     OrderStatus = "CLOSED"     //（关闭状态：授权未完成超时关闭或冻结资金全额解冻）
+)
+
 // FundAuthOperationDetailQueryRsp 资金授权操作查询接口响应参数
 type FundAuthOperationDetailQueryRsp struct {
 	Content struct {
-		Code                    Code   `json:"code"`
-		Msg                     string `json:"msg"`
-		SubCode                 string `json:"sub_code"`
-		SubMsg                  string `json:"sub_msg"`
-		AuthNo                  string `json:"auth_no"`
-		OutOrderNo              string `json:"out_order_no"`
-		TotalFreezeAmount       string `json:"total_freeze_amount"`
-		RestAmount              string `json:"rest_amount"`
-		TotalPayAmount          string `json:"total_pay_amount"`
-		OrderTitle              string `json:"order_title"`
-		PayerLogonId            string `json:"payer_logon_id"`
-		PayerUserId             string `json:"payer_user_id"`
-		ExtraParam              string `json:"extra_param"`
-		OperationId             string `json:"operation_id"`
-		OutRequestNo            string `json:"out_request_no"`
-		Amount                  string `json:"amount"`
-		OperationType           string `json:"operation_type"`
-		Status                  string `json:"status"`
-		Remark                  string `json:"remark"`
-		GMTCreate               string `json:"gmt_create"`
-		GMTTrans                string `json:"gmt_trans"`
-		PreAuthType             string `json:"pre_auth_type"`
-		TransCurrency           string `json:"trans_currency"`
-		TotalFreezeCreditAmount string `json:"total_freeze_credit_amount"`
-		TotalFreezeFundAmount   string `json:"total_freeze_fund_amount"`
-		TotalPayCreditAmount    string `json:"total_pay_credit_amount"`
-		TotalPayFundAmount      string `json:"total_pay_fund_amount"`
-		RestCreditAmount        string `json:"rest_credit_amount"`
-		RestFundAmount          string `json:"rest_fund_amount"`
-		CreditAmount            string `json:"credit_amount"`
-		FundAmount              string `json:"fund_amount"`
+		Code                    Code        `json:"code"`
+		Msg                     string      `json:"msg"`
+		SubCode                 string      `json:"sub_code"`
+		SubMsg                  string      `json:"sub_msg"`
+		AuthNo                  string      `json:"auth_no"`
+		OutOrderNo              string      `json:"out_order_no"`
+		OrderStatus             OrderStatus `json:"order_status"`
+		TotalFreezeAmount       string      `json:"total_freeze_amount"`
+		RestAmount              string      `json:"rest_amount"`
+		TotalPayAmount          string      `json:"total_pay_amount"`
+		OrderTitle              string      `json:"order_title"`
+		PayerLogonId            string      `json:"payer_logon_id"`
+		PayerUserId             string      `json:"payer_user_id"`
+		ExtraParam              string      `json:"extra_param"`
+		OperationId             string      `json:"operation_id"`
+		OutRequestNo            string      `json:"out_request_no"`
+		Amount                  string      `json:"amount"`
+		OperationType           string      `json:"operation_type"`
+		Status                  string      `json:"status"`
+		Remark                  string      `json:"remark"`
+		GMTCreate               string      `json:"gmt_create"`
+		GMTTrans                string      `json:"gmt_trans"`
+		PreAuthType             string      `json:"pre_auth_type"`
+		TransCurrency           string      `json:"trans_currency"`
+		TotalFreezeCreditAmount string      `json:"total_freeze_credit_amount"`
+		TotalFreezeFundAmount   string      `json:"total_freeze_fund_amount"`
+		TotalPayCreditAmount    string      `json:"total_pay_credit_amount"`
+		TotalPayFundAmount      string      `json:"total_pay_fund_amount"`
+		RestCreditAmount        string      `json:"rest_credit_amount"`
+		RestFundAmount          string      `json:"rest_fund_amount"`
+		CreditAmount            string      `json:"credit_amount"`
+		FundAmount              string      `json:"fund_amount"`
 	} `json:"alipay_fund_auth_operation_detail_query_response"`
 	Sign string `json:"sign"`
 }
